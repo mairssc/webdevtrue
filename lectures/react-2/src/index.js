3a5b63b55f9bfeb2cf96dce02f4bc52495076e53
unknown <lib_laptop@MOFF-ASUC-002.lib.berkeley.edu>
2021-10-14 21:48:34 -0700

    react-2 lecture

--- lectures/react-2/src/index.js
+++ lectures/react-2/src/index.js
@@ -1,21 +1,63 @@
-import React from 'react';
+
+import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square() {
+function Square(props) {
   return (
-    <button className="square">
-      {/* TODO */}
-    </button>
-  );
+    <button className="square" onClick={() => props.onClick()}>
+    {props.value}
+  </button>
+);
 }
 
 function Board() {
-const renderSquare = function(i) {
-  return <Square />;
-}
- 
-const status = 'Next player: X';
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsPlaying, setXIsPlaying] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+};
+
+  let winner = calculateWinner(squares);
+  let status = "Next player: " + (xIsPlaying ? "X" : "O");
+  if (winner) {
+    status = "Winner is " + winner;
+  }
+
+  const handleClick = (i) => {
+    const squares_copy = squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares_copy[i] = (xIsPlaying ? "X" : "O");
+    setSquares(squares_copy);
+    setXIsPlaying(!xIsPlaying);
+  }
+
+  const renderSquare = function(i) {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  }
 
   return (
     <div>
